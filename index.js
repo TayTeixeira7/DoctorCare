e5ed817d0d96a645dd7474fb891af07a39213e81
Taynara Teixeira <42852460+TayTeixeira7@users.noreply.github.com>
2024-02-04 23:23:24 -0300

    metade dos requisitos concluídos
    Criação da section Serviços, efeitos de transição e ações dos botões

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function fecharMenu() {
     document.body.classList.remove('menu_ativo')
 }
 
+ScrollReveal({
+    origin: 'top',  /* vai se movimentar no sentido do topo para baixo */
+    distance: '30px', /* vai se movimentar 30px */
+    duration: 700, /* vai durar 700ms para mostrar*/
+}).reveal('#home');
