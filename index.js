6515886fecc810c2c45c0a66c15ea92a4f4d5c88
Taynara Teixeira <42852460+TayTeixeira7@users.noreply.github.com>
2024-02-06 23:22:21 -0300

    parte mobile concluida

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+
 function onScroll() {
-    if(scrollY > 0) {
+    showNavOnScroll()
+    showBackToTopButtonOnScroll()
+}
+
+/* Função para mudar a cor da barra de navegação */
+function showNavOnScroll() {
+        if(scrollY > 0) {
         navegacao.classList.add('rolagem')
     } else {
         navegacao.classList.remove('rolagem')
     } 
+}
+
+/* Função para o botão flutuante */
+function showBackToTopButtonOnScroll() {
+    if(scrollY > 400) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
 }
 
 function abrirMenu() {
@@ -17,4 +36,4 @@ ScrollReveal({
     origin: 'top',  
     distance: '40px', 
     duration: 700,
-}).reveal(`#home, #home img, #home .container_numeros, #services, #services header, #services .card, #about, #about header, #about .content`);
+}).reveal(`#home, #home img, #home .container_numeros, #services, #services header, #services .card, #about, #about header, #about .content, #contact, #contact header, #contact .content`);
